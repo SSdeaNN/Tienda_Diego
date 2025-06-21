c16b1bb58514a9bbe8311990e0b9057379b28bdb
SSdeaNN <vegeto658@gmail.com>
2025-06-21 16:32:27 -0600

    Cambios de todo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next";
+import { Footer } from '@/components/Footer';
+import { Navbar } from '@/components/Navbar';
+import { Inter } from 'next/font/google';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
